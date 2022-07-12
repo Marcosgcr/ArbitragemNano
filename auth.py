1416686a8f9cf7ebfc4a6a7c9f24cb8e1b49936d
Marcos Guimarães <41563209+Marcosgcr@users.noreply.github.com>
2022-07-12 17:21:05 +0100

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,8 @@
+##Put here the credencials of Kraken and Binance API
+## Don´t share your private API key with anyone
+
+kraken_api_sec = "PUTHEREKRAKENAPISEC"
+lralen_api_key = "PUTHEREKRAKENAPIKEY"
+
+binance_api_sec = "PUTHEREBINANCEAPISEC"
+binance_api_key = "PUTHEREBINANCEAPIKEY"
