426ca225f7de6b6caab6f0760a07fc3889ff0b53
Marcos Guimarães <41563209+Marcosgcr@users.noreply.github.com>
2022-07-12 17:21:57 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@
 ## Don´t share your private API key with anyone
 
 kraken_api_sec = "PUTHEREKRAKENAPISEC"
-lralen_api_key = "PUTHEREKRAKENAPIKEY"
+Kralen_api_key = "PUTHEREKRAKENAPIKEY"
 
 binance_api_sec = "PUTHEREBINANCEAPISEC"
 binance_api_key = "PUTHEREBINANCEAPIKEY"
